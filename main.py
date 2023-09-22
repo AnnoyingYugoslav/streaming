eb2e1c2f94419565c9d34971544bd04eb0e46ed0
AnnoyingYugoslav <16bitplayer2003@gmail.com>
2023-09-22 18:59:06 +0200

    ye

--- main.py
+++ main.py
@@ -1,4 +1,54 @@
-name = "test"
-name2 = "test2"
-print(name)
-print(name2)
+import requests
+import shutil
+import os
+import time
+import pytesseract
+from PIL import Image
+
+def makeUrl(afterID, subreddit):
+    newUrl = subreddit.split('/.json')[0] + "/.json?after={}".format(afterID)
+    return newUrl
+
+def downloadImage(imageUrl, imageAmount, save_dir):
+    filename = os.path.join(save_dir, "1")
+    extension = imageUrl.split('.')[-1]  # Extract the file extension
+    if filename:
+        filename = f"{filename}.{extension}"  # Include the file extension
+        r = requests.get(imageUrl, stream=True)
+        with open(filename, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+            print("Successfully downloaded: " + imageUrl)
+            imageAmount += 1
+    return imageAmount
+
+def runDownload():
+    subreddit = "https://www.reddit.com/r/memes/new"
+    limit = 1
+    subJson = ''
+    x = 0
+    while x < limit:
+        if subJson:
+            url = makeUrl(subJson['data']['after'], subreddit)
+        else:
+            url = makeUrl('', subreddit)
+        subJson = requests.get(url, headers={'User-Agent': 'MyRedditScraper'}).json()
+        post = subJson['data']['children']
+        postCount = range(len(post))
+
+        for i in postCount:
+            imageUrl = (post[i]['data']['url'])
+            _imageUrls = []
+            _imageUrls.append(imageUrl)
+            x = downloadImage(_imageUrls[0], x, r'C:\Users\16bit\Desktop\syf z pulpitu\syf')
+            if x == limit:
+                break
+def textExtract():
+    path_to_tesseract = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    path_to_image = 'C:\\Users\\16bit\\Desktop\\syf z pulpitu\\syf\\1.jpg'
+    pytesseract.tesseract_cmd = path_to_tesseract
+    img = Image.open(path_to_image)
+    text = pytesseract.image_to_string(img)
+    print(text)
+
+
+textExtract()
